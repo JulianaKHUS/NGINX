09c0815d4d0d18f64291a306e97aec59a94c607a
otd6638 <iulianka99@mail.ru>
2024-11-25 18:12:25 +0300

    NGINX

---
+++ src/main/java/ru/netology/proxynginx/ProxyNginxApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.proxynginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyNginxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyNginxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/proxynginx/ProxyNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.proxynginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyNginxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
